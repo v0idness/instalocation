b75870925157a940b8754759b72e9d1e4cdef0f6
laura <rettigx@googlemail.com>
2013-11-26 19:35:14 +0100

    first commit

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,8 @@ class EventsController < ApplicationController
 
 	def create
 		# twitter magic happens here
-		@event = Event.new()
-  		@event.save
-  		redirect_to @event
+		#@event = Event.new()
+  		#@event.save
+  		#redirect_to @event
 	end
 end
