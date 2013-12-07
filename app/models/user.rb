f440df819459c422076c318a252cf553b5c783f0
Laurentiu <ion-laurentiu.manole@unifr.ch>
2013-12-07 15:14:13 +0100

    new file:   app/controllers/confirmation_controller.rb 	new file:   app/controllers/registrations_controller.rb 	new file:   app/controllers/users_controller.rb 	new file:   app/helpers/confirmation_helper.rb 	new file:   app/helpers/registrations_helper.rb 	new file:   app/helpers/users_helper.rb 	new file:   app/models/user.rb 	new file:   app/views/confirmation/index.html.erb 	new file:   app/views/devise/confirmations/new.html.erb 	new file:   app/views/devise/mailer/confirmation_instructions.html.erb 	new file:   app/views/devise/mailer/reset_password_instructions.html.erb 	new file:   app/views/devise/mailer/unlock_instructions.html.erb 	new file:   app/views/devise/passwords/edit.html.erb 	new file:   app/views/devise/passwords/new.html.erb 	new file:   app/views/devise/registrations/edit.html.erb~ 	new file:   app/views/devise/registrations/new.html.erb~ 	new file:   app/views/devise/sessions/new.html.erb 	new file:   app/views/devise/shared/_links.erb 	new file:   app/views/devise/unlocks/new.html.erb 	new file:   app/views/home/ind.html.erb 	new file:   app/views/registrations/new.html.erb 	new file:   app/views/users/_form.html.erb 	new file:   app/views/users/confirmations/new.html.erb 	new file:   app/views/users/edit.html.erb 	new file:   app/views/users/index.html.erb 	new file:   app/views/users/mailer/confirmation_instructions.html.erb 	new file:   app/views/users/mailer/reset_password_instructions.html.erb 	new file:   app/views/users/mailer/unlock_instructions.html.erb 	new file:   app/views/users/new.html.erb 	new file:   app/views/users/passwords/edit.html.erb 	new file:   app/views/users/passwords/new.html.erb 	new file:   app/views/users/registrations/edit.html.erb~ 	new file:   app/views/users/registrations/new.html.erb~ 	new file:   app/views/users/sessions/new.html.erb 	new file:   app/views/users/shared/_links.erb 	new file:   app/views/users/show.html.erb 	new file:   app/views/users/unlocks/new.html.erb 	new file:   config/initializers/devise.rb 	new file:   config/locales/devise.en.yml 	new file:   db/migrate/20131206231115_create_users.rb 	new file:   db/migrate/20131206231138_add_devise_to_users.rb 	new file:   public/stylesheets/scaffold.css 	new file:   test/fixtures/users.yml 	new file:   test/functional/confirmation_controller_test.rb 	new file:   test/functional/registrations_controller_test.rb 	new file:   test/functional/users_controller_test.rb 	new file:   test/unit/helpers/confirmation_helper_test.rb 	new file:   test/unit/helpers/registrations_helper_test.rb 	new file:   test/unit/helpers/users_helper_test.rb 	new file:   test/unit/user_test.rb

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :confirmable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :level
+  after_initialize :init
+
+    def init
+      self.level  ||= 0.0          
+    end
+end
