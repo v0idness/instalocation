cf4e1af531eeec8652d959bb93f51080f261d756
Laurentiu <ion-laurentiu.manole@unifr.ch>
2013-12-07 17:56:54 +0100

    modified:   app/controllers/users_controller.rb 	modified:   app/models/user.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
   # POST /users.xml
   def create
     @user = User.new(params[:user])
+  
     respond_to do |format|
       if @user.save
         format.html { redirect_to(@user, :notice => 'User was successfully created.') }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  validates_uniqueness_of :name
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :level
-  after_initialize :init
+  before_save :init
     def init
       self.level  ||= 0        
     end
