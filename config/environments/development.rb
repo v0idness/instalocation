26c691de904ef1449d122b76ce29a40285806fe2
Laurentiu <ion-laurentiu.manole@unifr.ch>
2013-12-12 22:53:04 +0100

    modified:   config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ Tweeporter::Application.configure do
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "localhost:3000",
+    :domain => "http://arcane-garden-3885.herokuapp.com",
     :authentication => :login,
     :user_name => "tweeporters@gmail.com",
     :password => "socialcomputing"
