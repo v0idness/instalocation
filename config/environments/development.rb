39351f277704890975cb02dd9cc47ebe61dca4da
Laurentiu <ion-laurentiu.manole@unifr.ch>
2013-12-12 22:44:46 +0100

    modified:   config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,6 +34,6 @@ Tweeporter::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'http://arcane-garden-3885.herokuapp.com' }
 end
 
