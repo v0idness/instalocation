c8bb41419228747ee4c86bdf813b21a55caef34f
Laurentiu <ion-laurentiu.manole@unifr.ch>
2013-12-18 12:31:35 +0100

    Events map added

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,23 @@ class LocationsController < ApplicationController
 	def index
 		@locs = Location.select(:country).map(&:country).uniq
 		@locs.sort_by!{ |char| char.downcase }
+		@ev = Event.all
+		@locations=Location.all
+		@hash = Gmaps4rails.build_markers(@locations) do |location, marker|
+  			marker.lat location.latitude
+  			marker.lng location.longitude
+  			@x = Event.find(location.id) 
+  			@a="<strong>#{@x.title}</strong>"
+  			@b="#{@x.text}"
+  			@c=""
+  			if @x.photos.first!=nil
+  				@c="<img src=#{@x.photos.first.image} width=\"70\" height=\"70\">"	
+			end
+  			box=[@a, @b, @c].join("\n")
+  			#marker.infowindow @x.text
+  			marker.infowindow box.gsub(/\n/, '<br />')
+  			marker.title @x.title
+  		end
 	end
 
 	def bycountry
